cc8dcfbfe7a76f9771bb07431723bb963de85e9f
yougotOWENd <0862456@hr.nl>
2017-04-06 13:20:06 +0200

    Add files via upload

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -11,9 +11,7 @@ namespace ConsoleApplication2
         static void Main(string[] args)
         {
             Console.WriteLine("hallo kanker boef");
-
-
-            Console.WriteLine("Nee");
+            Console.ReadKey();
         }
     }
 }
